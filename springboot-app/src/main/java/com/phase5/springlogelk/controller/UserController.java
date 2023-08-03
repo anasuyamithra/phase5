9e72a85a84633a689258c8b3cac9d39d57b23b97
Anasuya Mithra <anasuya.mithra@gmail.com>
2023-08-03 23:09:34 +0530

    spring code updated

--- springboot-app/src/main/java/com/phase5/springlogelk/controller/UserController.java
+++ springboot-app/src/main/java/com/phase5/springlogelk/controller/UserController.java
@@ -7,55 +7,37 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import com.phase5.springlogelk.SpringLogElkApplication;
-import com.phase5.springlogelk.model.User;
-import com.phase5.springlogelk.repository.UserRepository;
-
-import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/test")
 public class UserController {
-    private final UserRepository userRepository;
     
     Logger logger = LoggerFactory.getLogger(SpringLogElkApplication.class);
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+
+    @GetMapping("/hello")
+    public String hellopage() {
+        logger.info("Hello and welcome to the spring elk project!");
+        return "Hello and welcome to the spring elk project!";
+    }
+    
+    @GetMapping("/about")
+    public String aboutpage() {
+        logger.info("About page");
+        return "Hello and welcome to the about page!";
+    }
+    
+    @GetMapping("/signin")
+    public String signin() {
+        logger.info("Sign in Page");
+        return "Hello and welcome to the sign in page!";
+    }
+    
+    @GetMapping("/signout")
+    public String signout() {
+        logger.info("Sign out Page");
+        return "Hello and welcome to the sign out page!";
     }
 
-    @GetMapping
-    public List<User> getUsers() {
-        logger.info("Users Found.");
-        return userRepository.findAll();
-    }
-
-    @GetMapping("/{id}")
-    public User getUserById(@PathVariable Long id) {
-        logger.info("User Found.");
-        return userRepository.findById(id).orElse(null);
-    }
-
-    @PostMapping
-    public User createUser(@RequestBody User user) {
-        logger.info("Users Created.");
-        return userRepository.save(user);
-    }
-
-    @PutMapping("/{id}")
-    public User updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
-        User existingUser = userRepository.findById(id).orElse(null);
-        if (existingUser != null) {
-            existingUser.setName(updatedUser.getName());
-            existingUser.setEmail(updatedUser.getEmail());
-            logger.info("User Created.");
-            return userRepository.save(existingUser);
-        }
-        return null;
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Long id) {
-        logger.info("User Deleted.");
-        userRepository.deleteById(id);
-    }
+    
 }
--- springboot-app/src/main/java/com/phase5/springlogelk/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.phase5.springlogelk.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.phase5.springlogelk.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
